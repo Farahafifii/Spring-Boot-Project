e581414c897efe8aef2a10e7ab0fe90e019c82f3
Farah Afifi <farah-afifi@hotmail.com>
2024-09-01 11:41:04 +0300

    Task 69938 moved Database to Docker

--- src/main/java/com/task/sumerge/config/DatabaseConfig.java
+++ src/main/java/com/task/sumerge/config/DatabaseConfig.java
@@ -1,6 +1,5 @@
 package com.task.sumerge.config;
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -10,17 +9,16 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories(basePackages = "com.task.sumerge.repository")
-//@EntityScan(basePackages = "com.task.recommender.entity")
 public class DatabaseConfig {
 
     @Bean
 
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/course_db_jpa");
-        dataSource.setUsername("postgres");
-        dataSource.setPassword("postgres");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/courses_db_jpa");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
 
         return dataSource;
     }
